4d191b0cc1d930c4c638225fac5ce92fda50b5d8
deion <deion@deions-MacBook-Pro.local>
2023-10-08 17:28:52 -0700

    modify project routes to follow model

--- api/project_routes.py
+++ api/project_routes.py
@@ -10,16 +10,24 @@ def project_routes_wrapper(db):
     @project_blueprint.route("/save-project", methods=['POST'])
     def save_project():
         content = request.get_json()
-        project_id = content.get("project_id")
-        project_description = content.get("project_description")
-        organizer_data = content.get("organizer_data")
-        date_posted = content.get("date_posted")
-
         new_project = Projects(
-            project_id=project_id,
-            project_description=project_description,
-            organizer_data=organizer_data,
-            date_posted=date_posted
+            project_id=content.get("project_id"),
+            project_name=content.get("project_name"),
+            project_url_on_catalog=content.get("project_url_on_catalog"),
+            project_url_external=content.get("project_url_external"),
+            project_description=content.get("project_description"),
+            project_keywords=content.get("project_keywords"),
+            project_owners=content.get("project_owners"),
+            project_collaborators=content.get("project_collaborators"),
+            project_fields_of_science=content.get("project_fields_of_science"),
+            project_is_active=content.get("project_is_active", False),
+            project_geographical_scope=content.get("project_geographical_scope"),
+            project_targeted_groups=content.get("project_targeted_groups"),
+            projects_tasks=content.get("projects_tasks"),
+            project_started_date=content.get("project_started_date"),
+            project_email=content.get("project_email"),
+            date_posted=content.get("date_posted"),
+            organization_id=content.get("organization_id")
         )
         db.session.add(new_project)
         db.session.commit()
@@ -31,9 +39,22 @@ def project_routes_wrapper(db):
         if project:
             return jsonify({
                 'project_id': project.project_id,
+                'project_name': project.project_name,
+                'project_url_on_catalog': project.project_url_on_catalog,
+                'project_url_external': project.project_url_external,
                 'project_description': project.project_description,
-                'organizer_data': project.organizer_data,
-                'date_posted': now.strftime('%Y-%m-%d %H:%M:%S')
+                'project_keywords': project.project_keywords,
+                'project_owners': project.project_owners,
+                'project_collaborators': project.project_collaborators,
+                'project_fields_of_science': project.project_fields_of_science,
+                'project_is_active': project.project_is_active,
+                'project_geographical_scope': project.project_geographical_scope,
+                'project_targeted_groups': project.project_targeted_groups,
+                'projects_tasks': project.projects_tasks,
+                'project_started_date': project.project_started_date,
+                'project_email': project.project_email,
+                'date_posted': project.date_posted,
+                'organization_id': project.organization_id
             }), 200
         else:
             return jsonify(message="Project not found"), 404
@@ -45,10 +66,11 @@ def project_routes_wrapper(db):
             return jsonify(message="Project not found"), 404
 
         content = request.get_json()
-        project.project_description = content.get("project_description", project.project_description)
-        project.organizer_data = content.get("organizer_data", project.organizer_data)
-        project.date_posted = content.get("date_posted", project.date_posted)
-
+        # Updating all the fields based on the JSON received
+        for key, value in content.items():
+            if hasattr(project, key):  # This checks if the attribute exists in the Project model
+                setattr(project, key, value)
+        
         db.session.commit()
         return jsonify(message="Project updated successfully!"), 200
 
